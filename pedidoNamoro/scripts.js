482845fae740b34006aaf972a4ea500a9218bb08
CarlosGoncales <128001659+CarlosGoncales@users.noreply.github.com>
2025-11-10 21:37:16 -0300

    Update scripts.js

--- pedidoNamoro/scripts.js
+++ pedidoNamoro/scripts.js
@@ -1,17 +1,20 @@
-function accepted () {
-  alert("Você aceitou namorar comigo! :)");
+function accepted() {
+  alert(
+    "Você aceitou namorar com Carlos Gonçales," +
+      "por falor envie o número do seu dedo! e fotos para sugestão de Anliança"
+  );
   // redireciona para um URL após clicar no SIM
   location.href = "https://music.youtube.com/watch?v=izGwDsrQ1eQ";
 }
 
 function newPosition(min, max) {
-  return (Math.random() * (max - min) + min) + "%";
+  return Math.random() * (max - min) + min + "%";
 }
 
 function declined(btn) {
   // btn declarado na função
-  btn.style.position = 'absolute';
+  btn.style.position = "absolute";
   btn.style.bottom = newPosition(10, 90);
   btn.style.left = newPosition(10, 90);
-  console.log('ops......');
+  console.log("ops......");
 }
